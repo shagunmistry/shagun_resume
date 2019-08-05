ee7aee8f6893e20593c2f2f9d32942bc3dc64690
Tinqer <tinqer@tinqer.io>
2019-08-04 21:20:32 -0600

    cleanup

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 // DO NOT FORGET -- Change your Firbase rules to only allow authenticated write requests to your DB.
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyC_KKzYehksT1tdnSnreLHkgyKTU_ykL88",
-  authDomain: "shagunresume.firebaseapp.com",
-  databaseURL: "https://shagunresume.firebaseio.com",
-  projectId: "shagunresume",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "",
-  messagingSenderId: "702206149527",
+  messagingSenderId: "",
 };
 var firebaseApp = !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
 export default firebaseApp;
