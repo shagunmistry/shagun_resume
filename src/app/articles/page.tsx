0b04fa37c6f7a55a1eb8fe8bdc08471b36a21120
Shagun Mistry <shagun.mistry@hotmail.com>
2024-11-24 23:24:55 -0500

    Refactor articles page: simplify layout structure and add 'use client' directive to ArticleList component

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,5 +1,4 @@
 import { type Metadata } from 'next'
-
 import { SimpleLayout } from '@/components/SimpleLayout'
 import { getAllArticles } from '@/lib/articles'
 import { ArticlesList } from '@/components/ArticleList'
@@ -19,9 +18,7 @@ export default async function ArticlesIndex() {
       intro="This is where I write about my experiences, learnings, and thoughts on technology, sciences, startups, and life."
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-        <div className="flex max-w-3xl flex-col space-y-16">
-          <ArticlesList articles={articles} />
-        </div>
+        <ArticlesList articles={articles} />
       </div>
     </SimpleLayout>
   )
--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { motion } from 'motion/react'
 import { ArticleWithSlug } from '@/lib/articles'
 import Article from '@/components/Article'
