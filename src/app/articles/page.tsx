cb954250464acd5eb816a7f1486a9fa4af43ac80
Shagun Mistry <shagun.mistry@hotmail.com>
2024-11-24 23:10:21 -0500

    Comment out metadata export in articles page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -4,11 +4,11 @@ import { SimpleLayout } from '@/components/SimpleLayout'
 import { getAllArticles } from '@/lib/articles'
 import { ArticlesList } from '@/components/Article'
 
-export const metadata: Metadata = {
-  title: 'Articles',
-  description:
-    'This is where I write about my experiences, learnings, and thoughts on technology, sciences, startups, and life.',
-}
+// export const metadata: Metadata = {
+//   title: 'Articles',
+//   description:
+//     'This is where I write about my experiences, learnings, and thoughts on technology, sciences, startups, and life.',
+// }
 
 export default async function ArticlesIndex() {
   let articles = await getAllArticles()
