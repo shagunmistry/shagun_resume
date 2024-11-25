e476999fe414d41ec6814f1619bf8a2e944d4dca
Shagun Mistry <shagun.mistry@hotmail.com>
2024-11-24 23:29:30 -0500

    Refactor articles page: convert to client component, implement useEffect for article fetching, and comment out metadata export

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,16 +1,27 @@
+'use client'
 import { type Metadata } from 'next'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import { getAllArticles } from '@/lib/articles'
+import { ArticleWithSlug, getAllArticles } from '@/lib/articles'
 import { ArticlesList } from '@/components/ArticleList'
 
-export const metadata: Metadata = {
-  title: 'Articles',
-  description:
-    'This is where I write about my experiences, learnings, and thoughts on technology, sciences, startups, and life.',
-}
+// export const metadata: Metadata = {
+//   title: 'Articles',
+//   description:
+//     'This is where I write about my experiences, learnings, and thoughts on technology, sciences, startups, and life.',
+// }
 
-export default async function ArticlesIndex() {
-  let articles = await getAllArticles()
+import { useEffect, useState } from 'react'
+
+export default function ArticlesIndex() {
+  const [articles, setArticles] = useState<ArticleWithSlug[]>([])
+
+  useEffect(() => {
+    async function fetchArticles() {
+      let articles = await getAllArticles()
+      setArticles(articles)
+    }
+    fetchArticles()
+  }, [])
 
   return (
     <SimpleLayout
